a2694ecc8e58acee3c30ab8ed0d319683ce02356
URSUS32 <saul.razo@iteso.mx>
2024-09-24 20:58:40 -0600

    routes update, login view setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from '@components/layouts/header/header.component';
+// import { FooterComponent } from '@components/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path: '', redirectTo: 'home', pathMatch: 'full'},
+    // { path: 'home', component: HomeComponent },
+    {path: 'login', component: LoginComponent}
+];
