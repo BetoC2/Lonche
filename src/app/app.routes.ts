8b2c538050d07b79cb7fc061cabcf928ce2b13a3
Alberto <alberto.renteriac@iteso.mx>
2024-09-29 15:18:49 -0600

    Dashboard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from '@components/pages/landing-page/landing-page.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { DashboardComponent } from '@pages/dashboard/dashboard.component';
+import { ForYouComponent } from '@pages/dashboard/for-you/for-you.component';
+import { PopularComponent } from '@pages/dashboard/popular/popular.component';
+import { SavedComponent } from '@pages/dashboard/saved/saved.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: 'home', pathMatch: 'full'},
     // { path: 'home', component: HomeComponent },
-    {path: '', component: LandingPageComponent},
-    {path: 'landing', component: LandingPageComponent},
-    {path: 'login', component: LoginComponent}
+    { path: '', component: LandingPageComponent },
+    { path: 'landing', component: LandingPageComponent },
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'dashboard', component: DashboardComponent, children: [
+            { path: '', component: ForYouComponent },
+            { path: 'popular', component: PopularComponent },
+            { path: 'saved', component: SavedComponent }
+        ]
+    }
 ];
--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, Routes } from '@angular/router';
 import { HeaderComponent } from '@layouts/header/header.component';
 import { NavbarComponent } from '@layouts/navbar/navbar.component';
 import { LateralBarComponent } from '@layouts/lateral-bar/lateral-bar.component';
 import { NewPostComponent } from './new-post/new-post.component';
-import { ForYouComponent } from './for-you/for-you.component';
-import { PopularComponent } from './popular/popular.component';
-import { SavedComponent } from './saved/saved.component';
+import { RouterOutlet } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'for-you', component: ForYouComponent },
-  { path: 'popular', component: PopularComponent },
-  { path: 'saved', component: SavedComponent }
-]
 
 @Component({
   selector: 'app-dashboard',
