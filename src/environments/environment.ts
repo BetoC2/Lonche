8f0844a69dc24e5ebb7ff660dd196f088ce4dc7f
Alberto <alberto.renteriac@iteso.mx>
2024-11-25 16:50:14 -0600

    /

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://lonchebackend.onrender.com',
+  apiUrl: 'https://lonchebackend.onrender.com/',
   defaultPic:
     'https://lonche-bucket.s3.us-east-1.amazonaws.com/images/default.png',
 };
