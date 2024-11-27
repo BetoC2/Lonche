7b4fd32be642678125aa1ce2cd810a220a09206b
URSUS32 <saul.razo@iteso.mx>
2024-11-26 18:37:09 -0600

    minor

--- src/app/components/pages/dashboard/new-post/new-post.component.ts
+++ src/app/components/pages/dashboard/new-post/new-post.component.ts
@@ -107,7 +107,7 @@ export class NewPostComponent implements AfterViewInit {
             alert('Post creado con éxito');
             this.resetForm();
             window.location.reload();
-
+            
           },
           error: (error) => {
             console.error('Error al subir el archivo:', error);
